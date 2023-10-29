669e8921523b795cbae7b72295b6a49ba8ca2e91
thetonydavis <31522252+thetonydavis@users.noreply.github.com>
2023-10-29 15:40:06 -0700

    Update planbot.py

--- planbot.py
+++ planbot.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, redirect
 import json
 import uuid
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
@@ -9,16 +12,19 @@ user_data_store = {}
 
 @app.route('/receive_data', methods=['POST'])
 def receive_data():
-    print("POST data received:", request.form.to_dict())
+    logging.info("POST data received: %s", request.form.to_dict())
     data = request.form.to_dict()
     token = str(uuid.uuid4())
     user_data_store[token] = data
-    intermediary_url = f"https://chat-app-fa81b9.zapier.app/?token={token}"
+    intermediary_url = f"https://your-intermediary-page.com/?token={token}"
+    logging.info("Redirecting to: %s", intermediary_url)
     return redirect(intermediary_url)
 
 @app.route('/get_data/<token>', methods=['GET'])
 def get_data(token):
+    logging.info("Fetching data for token: %s", token)
     return json.dumps(user_data_store.get(token, {}))
 
 if __name__ == '__main__':
     app.run(debug=True)
+
