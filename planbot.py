07d85f45cbf36f08c5d0229f814a5b2b7d33e1d7
thetonydavis <31522252+thetonydavis@users.noreply.github.com>
2023-10-29 11:43:43 -0700

    Initial commit

---
+++ planbot.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, redirect
+import json
+import uuid
+
+app = Flask(__name__)
+
+# Temporary storage for user data
+user_data_store = {}
+
+@app.route('/receive_data', methods=['POST'])
+def receive_data():
+    data = request.form.to_dict()
+    token = str(uuid.uuid4())
+    user_data_store[token] = data
+    intermediary_url = f"https://your-intermediary-page.com/?token={token}"
+    return redirect(intermediary_url)
+
+@app.route('/get_data/<token>', methods=['GET'])
+def get_data(token):
+    return json.dumps(user_data_store.get(token, {}))
+
+if __name__ == '__main__':
+    app.run(debug=True)
