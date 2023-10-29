81b0478427914439e9994f4aee11ba4446561468
thetonydavis <31522252+thetonydavis@users.noreply.github.com>
2023-10-29 16:50:49 -0700

    Update planbot.py

--- planbot.py
+++ planbot.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
-# Temporary storage for user data
+# Temporary in-memory storage for user data
 user_data_store = {}
 
 @app.route('/receive_data', methods=['POST'])
@@ -17,23 +17,20 @@ def receive_data():
     token = str(uuid.uuid4())
     user_data_store[token] = data
     
-    # Change this to your chatbot URL
-    chatbot_url = "https://your_chatbot_url_here.com"
+    # Provided chatbot URL
+    chatbot_url = "https://chat-app-fa81b9.zapier.app/"
     
-    # Pass the token as a parameter
+    # Redirect with the token as a parameter
     intermediary_url = f"{chatbot_url}?token={token}"
-    
     logging.info("Redirecting to: %s", intermediary_url)
-    return redirect(intermediary_url)
+    
+    return redirect(intermediary_url, code=302)
 
 @app.route('/get_data/<token>', methods=['GET'])
 def get_data(token):
     logging.info("Fetching data for token: %s", token)
     data = user_data_store.get(token, {})
-    
-    # You can use the token to fetch data from within your chatbot
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run()
